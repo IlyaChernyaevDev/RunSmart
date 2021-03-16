aaf8fb56e40bdc1dbaa0d64360e62beb8081d6bf
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-03-16 05:41:38 +0300

    add -gilpgh-pages in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass         = require('gulp-sass');
 const rename       = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS     = require('gulp-clean-css');
+var ghPages        = require('gulp-gh-pages');
 
 // Static server
 gulp.task('server', function() {
@@ -30,6 +31,10 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('./build/**/*').pipe(ghPages());
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
